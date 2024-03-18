dd50be47cd396475b9264294a5514a18a802eebc
cquintana001 <cquintana002@gmail.com>
2024-03-18 18:49:18 +0100

    last commit 18032024

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -13,17 +13,17 @@ private:
     Span( void );
 
 public:
-    Span( unsigned int );
+    Span( unsigned int i);
     Span( const Span& obj);
     ~Span( void );
 
     Span&               operator=( const Span& rhs);
-    void                addNumber( int );
-    void                addNumber( std::list<int>::const_iterator, std::list<int>::const_iterator );
+    void                addNumber( int d);
+    void                addNumber( std::list<int>::const_iterator it_begin, std::list<int>::const_iterator it_end );
     unsigned int        longestSpan( void ) const;
     unsigned int        shortestSpan( void ) const;
 
     const std::list< int>*   getList( void ) const;
 };
 
-std::ostream& operator<<( std::ostream&, const Span& );
+std::ostream& operator<<( std::ostream& stream, const Span& span );
